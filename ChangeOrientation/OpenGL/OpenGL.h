40ca3c0b362c1992fa641d83fb7d3bad55b106d9
Ngoc-Tam LAM <lamngoctam@users.noreply.github.com>
2017-01-09 07:55:02 +0900

    Update OpenGL.h

--- ChangeOrientation/OpenGL/OpenGL.h
+++ ChangeOrientation/OpenGL/OpenGL.h
@@ -1,6 +1,5 @@
 //############################################################################
-//クラスの記述があるファイル
-//メンバ関数はOpenGL.cppに記述している
+//
 //############################################################################
 #ifndef _INCLUDED_OPENGL_H
 #define _INCLUDED_OPENGL_H
@@ -18,42 +17,41 @@
 
 
 //##################################
-//ウインドウを制御するクラス
-//窓毎に違う情報を記憶する
+//
 //##################################
 class OpenGL{
 
 public:
-	int	m_WindowWidth;		//ウィンドウの横幅
-	int	m_WindowHeight;		//ウィンドウの縦幅
-	int	m_State;						//マウスボタンが押されているか離されているか
-	int	m_Button;					//左クリックか中クリックか右クリックか
-	int	m_mouseLastX;			//直前マウス位置X
-	int	m_mouseLastY;			//直前マウス位置Y
-	int	m_mouseCurrentX;		//現在マウス位置X
-	int	m_mouseCurrentY;		//現在マウス位置Y
-	double	m_Aspect;				//現在のウインドウのアスペクト比
-	double	m_ObjectDepth;		//選択物体の奥行き深さ
+	int	m_WindowWidth;		
+	int	m_WindowHeight;		
+	int	m_State;						
+	int	m_Button;					
+	int	m_mouseLastX;			
+	int	m_mouseLastY;			
+	int	m_mouseCurrentX;		
+	int	m_mouseCurrentY;		
+	double	m_Aspect;				
+	double	m_ObjectDepth;		
 
-	//古い視点操作用
-	double  m_Rotate[3];	 //回転量(x,y,z)
-	double  m_EyePos[3];	 //視点位置(x,y,z)
+	//
+	double  m_Rotate[3];	 
+	double  m_EyePos[3];	
 
-	//新しい視点操作用（POV-Rayのカメラ座標系を参考にしました。）
-	CVector3d	m_Center;	//注目点
-	double		m_EyeDis;	//視点から注目点までの距離
-	CVector3d	m_Upper;	//上方向
-	CVector3d	m_Front;		//前方向
-	CVector3d	m_Right;		//右方向	
-	CVector3d	m_Eye;		//視点
+	//
+	CVector3d	m_Center;	
+	double		m_EyeDis;	
+	CVector3d	m_Upper;	
+	CVector3d	m_Front;		
+	CVector3d	m_Right;			
+	CVector3d	m_Eye;		
 
 
-	//コンストラクタ
+	
 	OpenGL() : m_WindowWidth(800), m_WindowHeight(800), 
 		m_State(GLUT_UP),   m_Button(NONE),
 		m_mouseLastX(0),    m_mouseLastY(0) 
 	{
-		//古い視点操作用
+		//
 		m_Rotate[0]=0.0; m_Rotate[1]=0.0; m_Rotate[2]=0.0;
 		m_EyePos[0]=0.0; m_EyePos[1]=0.0; m_EyePos[2]=10.0;
 
@@ -64,9 +62,9 @@ public:
 		m_Upper = CVector3d(0.0, 1.0, 0.0);
 	}
 
-	//メンバ関数
-	void Tracking( int, int, int, int );	//マウストラッキング、ボタンを押すと呼ばれる
-	void MovingMouse( int, int );			//ボタンを押しつつマウスを動かすと呼ばれる
+	
+	void Tracking( int, int, int, int );	
+	void MovingMouse( int, int );			
 	void SetEyePosition();
 	void DoMouseWheel(int direction);
 	void SetEyes(){
@@ -75,11 +73,11 @@ public:
 	}
 
 	//############################
-	//	OpenGLPicking	物体の選択
+	//	OpenGLPicking
 	//############################
-	CVector3d *ObjectPosition;		//選択した物体の位置
-	CVector3d ObjectTemp;			//何も選択しなかったときのObjectPositionの受け皿
-	void (*PickObject)( int x, int y, int Button );		//物体選択関数の関数ポインタ
+	CVector3d *ObjectPosition;		
+	CVector3d ObjectTemp;			
+	void (*PickObject)( int x, int y, int Button );		
 };
 
 
